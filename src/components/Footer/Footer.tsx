295b203cf7bcba0bbf1be9c20ac1618b0724cd2f
erik <erikhendrikson@hotmail.com>
2021-09-23 14:24:38 +0200

    adjust to PR

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,6 @@ import facebook from '../../assets/facebook.svg';
 import instagram from '../../assets/instagram.svg';
 import logo from '../../assets/logo.png';
 import twitter from '../../assets/twitter.svg';
-import { Wrapper } from '../Wrapper/Wrapper';
 
 export const Footer: React.FC = () => {
   const socialItems = [
@@ -17,95 +16,92 @@ export const Footer: React.FC = () => {
   ];
 
   return (
-    <div className="l-footer">
-      <Wrapper>
-        <div className="c-footer">
-          <div className="c-footer__logo">
-            <Image objectFit="contain" alt="logo" src={logo} />
-          </div>
-          <div className="c-footer__content">
-            <div className="c-footer__newsletter">
-              <ul className="c-footer__list">
-                <li>
-                  Op de hoogte blijven van Goodplace? Schrijf je vrijblijvend in
-                  voor de nieuwsbrief.
-                </li>
-                <li>
-                  <label htmlFor="e-mail">
-                    E-mailadres
-                    <input
-                      name="e-mail"
-                      className="c-footer__e-mail"
-                      placeholder="E-mailadres"
-                    />
-                  </label>
-                </li>
-              </ul>
-            </div>
-
-            <div className="c-footer__menu-lists">
-              <ul className="c-footer__list">
-                <li className="c-footer__label">Menu</li>
-                <li>
-                  <Link href="#">Over Goodplace</Link>
-                </li>
-                <li>
-                  <Link href="#">Veelgestelde vragen</Link>
-                </li>
-                <li>
-                  <Link href="#">Contact</Link>
-                </li>
-              </ul>
-
-              <ul className="c-footer__list">
-                <li className="c-footer__label">Kom in contact</li>
-                <li>
-                  +31 70 304 3665 <br /> hallo@goodplace.nl
-                </li>
-                <li>
-                  Prinses Margrietplantsoen 33 <br /> 2595 AM Den Haag <br />{' '}
-                  Nederland
-                </li>
-              </ul>
-
-              <ul className="c-footer__list">
-                <li className="c-footer__label">Legal</li>
-                <li>
-                  Non profit organisatie <br />
-                  RSIN 862391891
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="c-footer__legal-socials">
-            <div className="c-footer__legal">
-              <div>&copy; 2021 Goodplace. Alle rechten voorbehouden.</div>
-              <Link href="#">
-                <a className="c-footer__legal-links">Privacy beleid</a>
-              </Link>
-              <Link href="#">
-                <a className="c-footer__legal-links">Algemene voorwaarden</a>
-              </Link>
-            </div>
-            <div className="c-footer__socials">
-              {socialItems.map((item) => {
-                return (
-                  <Link key={item.id} href={item.href} passHref>
-                    <a>
-                      <Image
-                        width="20"
-                        height="20px"
-                        alt="logo"
-                        src={item.src}
-                      />
-                    </a>
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
+    <footer className="c-footer">
+      <div className="c-footer__logo">
+        <Image objectFit="contain" alt="logo" src={logo} />
+      </div>
+      <div className="c-footer__content">
+        <div className="c-footer__newsletter">
+          <ul className="c-footer__list">
+            <li>
+              Op de hoogte blijven van Goodplace? Schrijf je vrijblijvend in
+              voor de nieuwsbrief.
+            </li>
+            <li>
+              <label htmlFor="e-mail">
+                E-mailadres
+                <input
+                  name="e-mail"
+                  className="c-footer__e-mail"
+                  placeholder="E-mailadres"
+                />
+              </label>
+            </li>
+          </ul>
         </div>
-      </Wrapper>
-    </div>
+
+        <div className="c-footer__menu-lists">
+          <ul className="c-footer__list">
+            <li>
+              <strong>Menu</strong>
+            </li>
+            <li>
+              <Link href="#">Over&nbsp;Goodplace</Link>
+            </li>
+            <li>
+              <Link href="#">Veelgestelde&nbsp;vragen</Link>
+            </li>
+            <li>
+              <Link href="#">Contact</Link>
+            </li>
+          </ul>
+
+          <ul className="c-footer__list">
+            <li>
+              <strong>Kom in contact</strong>
+            </li>
+            <li>
+              +31 70 304 3665 <br /> hallo@goodplace.nl
+            </li>
+            <li>
+              Prinses Margrietplantsoen 33 <br /> 2595 AM Den Haag <br />{' '}
+              Nederland
+            </li>
+          </ul>
+
+          <ul className="c-footer__list">
+            <li>
+              <strong>Legal</strong>
+            </li>
+            <li>
+              Non profit organisatie <br />
+              RSIN 862391891
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="c-footer__legal-socials">
+        <div className="c-footer__legal">
+          <div>&copy; 2021 Goodplace. Alle rechten voorbehouden.</div>
+          <Link href="#">
+            <a className="c-footer__legal-links">Privacy beleid</a>
+          </Link>
+          <Link href="#">
+            <a className="c-footer__legal-links">Algemene voorwaarden</a>
+          </Link>
+        </div>
+        <div className="c-footer__socials">
+          {socialItems.map((item) => {
+            return (
+              <Link key={item.id} href={item.href} passHref>
+                <a>
+                  <Image width="20" height="20px" alt="logo" src={item.src} />
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </footer>
   );
 };
--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 import { Footer } from '../components/Footer/Footer';
 import { Navigation } from '../components/Navigation/Navigation';
+import { Section } from '../components/Section/Section';
+import { Wrapper } from '../components/Wrapper/Wrapper';
 import { AppConfig } from '../utils/AppConfig';
 import { Meta } from '../utils/Meta';
 
 const Base: React.FC = ({ children }) => (
-  <div className="antialiased text-black font-cabin">
+  <div className="antialiased text-black font-jost">
     <Meta appConfig={AppConfig} />
     <Navigation logo="https://via.placeholder.com/150" />
     {children}
-    <Footer />
+    <Section>
+      <Wrapper>
+        <Footer />
+      </Wrapper>
+    </Section>
   </div>
 );
 
