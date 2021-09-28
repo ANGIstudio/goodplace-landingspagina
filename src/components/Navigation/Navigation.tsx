d9937f2032de0b73eb7178412ad035b165f0f7e4
erik <erikhendrikson@hotmail.com>
2021-09-28 15:57:36 +0200

    adjust navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,46 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
 
+import closed from '../../assets/closed.svg';
+import hamburger from '../../assets/hamburger.svg';
 import { Button } from '../Button/Button';
 
 interface NavigationProps {
   logo: string;
+  mobilelogo: string;
 }
 
-export const Navigation: React.FC<NavigationProps> = ({ logo }) => {
+export const Navigation: React.FC<NavigationProps> = ({ logo, mobilelogo }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="c-navigation">
-      <div>
-        <Link href="#" passHref>
-          <a>
-            <Image className="c-navigation__logo" alt="logo" src={logo} />
-          </a>
-        </Link>
+      <div className="c-navigation__bar">
+        <div className="c-navigation__logo">
+          <Link href="#" passHref>
+            <a>
+              <Image alt="logo" src={logo} />
+            </a>
+          </Link>
+        </div>
+
+        <div className="c-navigation__mobile-logo">
+          <Link href="#" passHref>
+            <a>
+              <Image alt="logo" src={mobilelogo} />
+            </a>
+          </Link>
+        </div>
+
+        <nav>
+          <ul className="c-navigation__desktop">
+            <li className="c-navigation__desktop-item">
+              <Link href="#">Wat is het?</Link>
+            </li>
+            <li className="c-navigation__desktop-item">
+              <Link href="#">Hoe werkt het?</Link>
+            </li>
+            <li className="c-navigation__desktop-item">
+              <Link href="#">Contact</Link>
+            </li>
+            <li className="c-navigation__desktop-item">
+              <Button
+                tag="link"
+                href="#"
+                label="Download"
+                variant="green"
+                size="M"
+              />
+            </li>
+          </ul>
+
+          {isOpen ? (
+            <ul className="c-navigation__mobile">
+              <li className="c-navigation__mobile-item">
+                <Link href="#">Wat is het?</Link>
+              </li>
+              <li className="c-navigation__mobile-item">
+                <Link href="#">Hoe werkt het?</Link>
+              </li>
+              <li className="c-navigation__mobile-item">
+                <Link href="#">Contact</Link>
+              </li>
+              <li className="c-navigation__mobile-item">
+                <Button
+                  tag="link"
+                  href="#"
+                  label="Download"
+                  variant="green"
+                  size="M"
+                />
+              </li>
+            </ul>
+          ) : null}
+        </nav>
+
+        <button
+          type="button"
+          onClick={handleClick}
+          className="c-navigation__hamburger"
+        >
+          <Image alt="hamburger" src={isOpen ? closed : hamburger} />
+        </button>
       </div>
-      <nav>
-        <ul className="c-navigation__list">
-          <li className="c-navigation__list-item">
-            <Link href="#">Over Goodplace</Link>
-          </li>
-          <li className="c-navigation__list-item">
-            <Link href="#">Veelgestelde vragen</Link>
-          </li>
-          <li className="c-navigation__list-item">
-            <Link href="#">Contact</Link>
-          </li>
-          <li className="c-navigation__list-item">
-            <Button
-              tag="link"
-              href="#"
-              label="Download"
-              variant="green"
-              size="M"
-            />
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import logo from '../assets/logo.svg';
+import mobilelogo from '../assets/mobilelogo.svg';
 import { Footer } from '../components/Footer/Footer';
 import { Navigation } from '../components/Navigation/Navigation';
 import { Section } from '../components/Section/Section';
@@ -11,7 +12,7 @@ import { Meta } from '../utils/Meta';
 const Base: React.FC = ({ children }) => (
   <div className="antialiased text-black font-jost">
     <Meta appConfig={AppConfig} />
-    <Navigation logo={logo} />
+    <Navigation logo={logo} mobilelogo={mobilelogo} />
     {children}
     <Section>
       <Wrapper>
