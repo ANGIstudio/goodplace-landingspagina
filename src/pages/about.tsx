8d513d0082615072d8f141ede70cf97fd8175f45
Thomas Hoenselaars <thomas@angistudio.com>
2021-09-30 14:20:32 +0200

    changed color and padding about hero

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -53,27 +53,25 @@ const About: NextPage = () => {
 
   return (
     <Base>
-      <Section noPaddingBottom noPaddingTop>
+      <Section noPaddingTop bgColor="lightgreen">
         <Wrapper>
-          <div className="bg-abouthero bg-no-repeat bg-top md:bg-right">
-            <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-10">
-              <div className="md:max-w-lg">
-                <h2 className="mb-4 font-bold text-darkgreen">
-                  Leuk om kennis te maken
-                </h2>
-                <p className="text-green">
-                  Samen met jou vergroten we maatschappelijke betrokkenheid. We
-                  maken doneren voor iedereen leuk en laagdrempelig, door mensen
-                  eenvoudig en positief bij goede doelen en projecten te
-                  betrekken.
-                </p>
-              </div>
-              <img
-                className="w-full md:w-1/2 mt-8"
-                alt="awd"
-                src={imageframe.src}
-              />
+          <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-10">
+            <div className="md:max-w-lg">
+              <h2 className="mb-4 font-bold text-darkgreen">
+                Leuk om kennis te maken
+              </h2>
+              <p className="text-green">
+                Samen met jou vergroten we maatschappelijke betrokkenheid. We
+                maken doneren voor iedereen leuk en laagdrempelig, door mensen
+                eenvoudig en positief bij goede doelen en projecten te
+                betrekken.
+              </p>
             </div>
+            <img
+              className="w-full md:w-1/2 mt-8"
+              alt="awd"
+              src={imageframe.src}
+            />
           </div>
         </Wrapper>
       </Section>
